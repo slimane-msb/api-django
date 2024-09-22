32ed6fc20be64eccea37c7a047a9e024a4b761aa
slimane-msb <slimanemesbah0@gmail.com>
2024-09-22 17:32:24 +0200

    compose seprate not possible

--- django-site/build.sh
+++ django-site/build.sh
@@ -1,8 +1,2 @@
-
-docker rm -vf $(docker ps -aq)
-docker rmi -f $(docker images -aq)
-docker system prune -y
-docker volume prune -y
-docker volume rm -f $(docker volume ls)
 cp env-exemple .env
 sudo docker-compose up
